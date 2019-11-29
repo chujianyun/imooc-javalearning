f4a17eb611def259dc7b813b63a1227a6289c0cf
rumingruyue <liuwangyangedu@163.com>
2019-11-29 23:47:11 +0800

    最新

--- src/main/java/com/imooc/basic/learn_exit/ExitTest.java
+++ src/main/java/com/imooc/basic/learn_exit/ExitTest.java
@@ -1,4 +1,23 @@
 package com.imooc.basic.learn_exit;
 
+import java.io.IOException;
+
 public class ExitTest {
+    public static void main(String[] args) {
+        // 修改 SecurityManager
+        System.setSecurityManager(new SecurityManager() {
+            @Override
+            public void checkExit(int status) {
+                throw new SecurityException("不允许退出");
+            }
+        });
+
+        try {
+            throw new IOException("IO异常了");
+        } catch (IOException e) {
+            System.exit(2);
+        } finally {
+            System.out.println("Exiting the program");
+        }
+    }
 }
